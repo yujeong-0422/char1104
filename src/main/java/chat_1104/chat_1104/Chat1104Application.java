e86981005ec76f0031387e3e19ec9352279dcb5a
yujeong-0422 <pride0422@naver.com>
2023-11-04 13:16:59 +0900

    first commit

---
+++ src/main/java/chat_1104/chat_1104/Chat1104Application.java
@@ -0,0 +1,13 @@
+package chat_1104.chat_1104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat1104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat1104Application.class, args);
+	}
+
+}
---
+++ src/test/java/chat_1104/chat_1104/Chat1104ApplicationTests.java
@@ -0,0 +1,13 @@
+package chat_1104.chat_1104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat1104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
